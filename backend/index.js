e53e84de810412631ce825dd0b34d04e7b206d2b
MajaR46 <maja.rotovnik002@gmail.com>
2024-10-14 09:35:50 +0200

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,27 @@ const reviewRoutes = require("./routes/review");
 const jobRoutes = require("./routes/job");
 const projectRoutes = require("./routes/project");
 
-app.use(cors());
+const allowCors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Another common pattern
+  // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+};
+app.use(allowCors);
 app.use(express.json());
 
 connectDB();
